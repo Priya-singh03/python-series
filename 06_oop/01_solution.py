b1afcfe0ecfc5f6062912bbf23b62796010f457e
priya singh <priyakushwaha83036@gmail.com>
2025-09-06 09:55:35 +0530

    decorators

---
+++ 06_oop/01_solution.py
@@ -0,0 +1,60 @@
+#create a car class with attributes like brand and model, then create an instance of this class
+
+class Car:
+    
+    def __init__(self, brand , model):
+        self.__brand = brand
+        self.__model = model
+
+    def get_brand(self):
+        return self.__brand + " !"  
+
+
+    def full_name(self):
+        return f"{self.__brand} {self.__model}"
+
+    def fuel_type(self):
+        return "Petrol or Diesel"
+    
+    @staticmethod
+    def general_description():
+        return "Cars are a means of transport"
+
+    @property
+    def model(self):
+        return self.__model
+
+
+class ElectricCar(Car):
+    def __init__(self, brand, model, battery_size ):
+        super().__init__(brand, model)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Electric charge"
+
+my_tesla = ElectricCar("Tesla", "Model S", "85kWh")
+#print(my_tesla.model)
+#print(my_tesla.__brand)
+#print(my_tesla.get_brand())
+print(my_tesla.fuel_type())
+
+#safari = Car("Tata", "Safari")
+#print(safari.fuel_type())
+
+my_car = Car("Tata", "Safari")
+#my_car.model = "City"
+print(my_car.model)
+Car("Tata", "Nexon")
+
+
+#print(my_car.general_desciption())
+#print(Car.general_description())
+
+
+
+#print(my_tesla.full_name())        
+#my_car = Car("Toyota", "corolla")
+#print(my_car.brand)
+#print(my_car.model)
+#print(my_car.full_name)
